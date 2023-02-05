eb65e6a9bfcc75ca71da2695b63d6c81cb9e8402
mkoksharova <110388813+mkoksharova@users.noreply.github.com>
2023-02-05 09:59:40 +0300

    seminar2_homework

---
+++ python_course/python_lection1.py
@@ -0,0 +1,29 @@
+# n = 5
+# test_string = 'hello'
+# print(n) # вывод переменных
+# print(type(test_string)) # вывод типа данных
+
+# m = 'gdsg\'ds'
+# print(m)
+
+#Интерполяция
+# a = 3
+# b = 5.6
+# c = 'or'
+# print(a, '-', b, '-', c)
+# print(f'{a} - {b} - {c}')
+# print('{} - {} - {}'.format(a,b,c))
+# Ввод данных
+# print('Введите первое число!')
+# u = input()
+# print(u)
+
+# v =input('Введите второе число:')
+# print(v)
+
+c = 5.09
+print(c)
+print(type(c))
+c = str(c)
+print(c)
+print(type(c))
---
+++ python_course/python_sem1.py
@@ -0,0 +1,26 @@
+#1
+# n = int(input('Введите кол-во км, которые машина проходит за день: '))
+# m = int(input('Введите общий пробег: '))
+# print((m + n - 1) // n)
+#2
+# first_class = int(input('Введите первое число: '))
+# second_class = int(input('Введите второе число: '))
+# third_class = int(input('Введите третье число: '))
+# part = 2
+# first_part = (first_class + 1) // part
+# second_part = (second_class + 1) // part
+# third_part = (third_class + 1) // part    
+
+# print(first_part + second_part + third_part)
+#3
+# i = int(input('Введите номер вагона в вкоторый сел Витя: '))
+# j = int(input('Введите номер вагона:'))
+
+# result = (i + j - 1)
+# print(result)
+#4
+# year = int(input('Введите год: '))
+# if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+#     print('YES')
+# else:
+#     print('NO')
