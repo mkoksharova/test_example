a3df1786b28a55cffb1d6b5eb65d958cb153ff5d
mkoksharova <110388813+mkoksharova@users.noreply.github.com>
2023-02-24 14:47:40 +0300

    save task30

---
+++ python_course/python_sem6.py
@@ -0,0 +1,22 @@
+
+# n = int(input())
+# list_first = list()
+# for i in range(n):
+#     x = int(input())
+#     list_first.append(x)
+#     count = 0
+# for i in range(1, n - 1):
+#     if list_first[i - 1] < list_first[i] < list_first[i + 1]:
+#         count += 1
+# print(count)
+
+# #2
+# numbers = [5, 1, 5, 3, 6, 3, 4]
+# count= 0
+# for i in range(1, len(numbers)-1):
+#     if numbers[i - 1] < numbers[i] > numbers[i+1]:
+#         count += 1
+
+
+
+
